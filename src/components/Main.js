017f8f0fada75af57d9ac6bf1423c02300412212
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-09-24 15:20:39 +0300

    They do know. Closes #5

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,7 +17,7 @@ class Main extends Component {
           <Route exact path="/" component={(props) => <Article {...props} pages={this.props.pages} file="/docs/index.md" />} />
           <Route path="/about" component={About} />
 
-          <Route path="/docs/:folder/:filename" component={(props) => <Article {...props} pages={this.props.pages} />} />
+          <Route path="/docs/:folder*/:filename" component={(props) => <Article {...props} pages={this.props.pages} />} />
           <Route path="/docs/:folder" component={(props) => <Article {...props} pages={this.props.pages} />} />
         </Switch>
       </main>
