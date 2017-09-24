6ed298df65d0120c44c6567ce2612be4ce126fb1
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-09-24 16:41:30 +0300

    Enable travis

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Main from './components/Main';
+import { BrowserRouter } from 'react-router-dom';
+
+Object.defineProperty(window, "matchMedia", {
+    value: jest.fn(() => { return { matches: true } })
+});
+
+Object.defineProperty(window, "requestAnimationFrame", {
+    value: jest.fn(() => { return { matches: true } })
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <BrowserRouter>
+      <Main pages={[{filename: "./Advanced Custom Fields/Options page.md", path: "/static/media/Options page.5baf205a.md"}]} />
+    </BrowserRouter>,
+    div
+  );
 });
