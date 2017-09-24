eed973dffa6e03fb0b7043608b6a4ba7ca6f1095
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-09-24 03:23:38 +0300

    Make navigation nice for everyone

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import style from './Navigation.module.styl';
+import { stripStartAndEnd } from '../lib/pages'
 
 class Navigation extends Component {
     render() {
-      console.log(this.props);
       return (
         <nav className={style.siteNavigation} id={this.props.id}>
           {this.props.children}
 
           <ul>
           {this.props.pages.map((page) => (
-            <li>
-              <NavLink to={page.filename.replace('./', '/docs/')}>{page.filename}</NavLink>
+            <li key={page.filename}>
+              <NavLink to={page.filename.replace('./', '/docs/')}>{stripStartAndEnd(page.filename)}</NavLink>
             </li>
           ))}
           </ul>
