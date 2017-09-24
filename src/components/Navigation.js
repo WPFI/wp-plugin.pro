7d661b50f088814bbf9ba560d2527af90ce281ab
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-09-24 14:57:31 +0300

    Properly build filetree

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -11,78 +11,96 @@ class Navigation extends Component {
 
       this.state = {
         tree: {},
+        treeGenInProgress: false,
       };
 
     }
 
     buildFolderList() {
-      const folders = this.props.pages.reduce((a, page) => {
-        console.log(page);
-        const parts = breakIntoParts(page.filename).slice(1);
-        const addDir = (name) => ({ type: 'dir', name });
-        const addFile = (name) => ({ type: 'file', name, path: page.path, filename: page.filename });
-        const getPrev = (stage) => stage === 0 ? false : parts[stage - 1];
+      this.setState({
+        treeGenInProgress: true,
+      }, () => {
+          const filetree = this.props.pages.reduce((a, page) => {
+          const parts = breakIntoParts(page.filename).slice(1);
+          const addDir = (name) => ({ type: 'dir', name });
+          const addFile = (name) => ({ type: 'file', name, path: page.path, filename: page.filename });
 
+          const tree = parts.reduceRight((prev, cur) => {
+            if (prev === null) {
+              return addFile(cur);
+            } else {
+              return { ...addDir(cur), [prev.name]: prev };
+            }
+          }, null);
 
-              /* const arr = ['Folder', 'Subfolder', 'file.md'];
-              const tree = {
-                Folder: {
-                  type: 'dir',
-                  name: 'Folder', // debugging purposes jne
-
-                  Subfolder: {
-                    type: 'dir',
-                    name: 'Subfolder',
-
-                    'file.md': {
-                      type: 'file',
-                      name: 'file.md',
-                      fetch: 'http://...'
-                    }
-                  }
-                }
-              } */
-
-        const tree = parts.reduce((b, part, stage) => {
-          switch (stage) {
-            case 0:
-              b[part] = isDir(part) ? addDir(part) : addFile(part);
-            break;
-            case 1:
-              b[getPrev(stage)][part] = isDir(part) ? addDir(part) : addFile(part);
-            break;
-            case 2:
-             b[getPrev(stage - 1)][getPrev(stage)][part] = isDir(part) ? addDir(part) : addFile(part);
-            break;
-          }
-
-          return b;
+            // console.log(tree);
+          // a[parts[0]] = tree;
+          return deepmerge(a, { [parts[0]]: tree });
         }, {});
 
-        console.log(tree);
+        // console.log(filetree);
 
-        return deepmerge(a, tree);
-      }, {});
-
-      console.log(folders);
+        this.setState({
+          tree: filetree,
+          treeGenInProgress: false,
+        });
+      })
     }
 
-    componentDidUpdate() {
-      this.buildFolderList();
+    componentWillReceiveProps(newProps) {
+      if (this.props !== newProps) {
+        this.buildFolderList();
+      }
     }
 
     render() {
+      const naviLink = (item) => {
+        return item.filename
+          ? <NavLink to={item.filename.replace('./', '/docs/')}>{item.name}</NavLink>
+          : item.name
+      }
+      const nextStage = (tree) => {
+        const keys = Object.keys(tree);
+
+        console.log(keys.length, keys);
+        if (keys.length < 3) {
+          return false;
+        }
+
+        return (
+          <ul>
+            {keys.map((key) => {
+              if (key === 'name' || key === 'type') {
+
+              } else {
+                const t = tree[key];
+                return (
+                  <li>
+                    {naviLink(t)}
+                    {typeof t !== 'string' ? nextStage(t) : ''}
+                  </li>
+                );
+              }
+            })}
+          </ul>
+        );
+      };
+
       return (
         <nav className={style.siteNavigation} id={this.props.id}>
           {this.props.children}
 
           <ul>
-          {this.props.pages.map((page) => (
-            <li key={page.filename}>
-              <NavLink to={page.filename.replace('./', '/docs/')}>{stripStartAndEnd(page.filename)}</NavLink>
-            </li>
-          ))}
+            {Object.keys(this.state.tree).map((key) => {
+              const tree = this.state.tree[key];
+              console.log(tree);
+              return <li>
+                {naviLink(tree)}
+                {nextStage(tree)}
+              </li>
+            })}
           </ul>
+
         </nav>
       );
   }
