123403e06b1100e9a407e2193308e18d54ee9250
Christian Nikkanen <me@kisu.li>
2017-10-08 15:25:28 +0300

    Maybe docs isn't unwanted

--- src/lib/filetree.js
+++ src/lib/filetree.js
@@ -60,7 +60,7 @@ class filetree {
 
     // Pass 3 -- remove unwanted dirs
     Object.keys(dirs).forEach(dirName => {
-      if (['/docs', '/'].indexOf(dirName) > -1) {
+      if (['/'].indexOf(dirName) > -1) {
         delete dirs[dirName];
       }
     });
